48b12b0332b29f7e1bae08abb38b2472296c786e
kiryl-bahdanovich <work.kiryl.bahdanovich@gmail.com>
2016-02-13 13:37:43 +0300

    add share to twitter script

--- scripts/page.js
+++ scripts/page.js
@@ -2,3 +2,8 @@
     ga('send', 'event', 'social', 'share', 'facebook');
     return true;
 }
+
+function GAShareToTwitter() {
+    ga('send', 'event', 'social', 'share', 'twitter');
+    return true;
+}
